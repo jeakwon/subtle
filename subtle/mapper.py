d3b51bdad38f51820d759639e39bbabd2a2e4766
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-01-05 17:05:49 +0900

    update

--- subtle/mapper.py
+++ subtle/mapper.py
@@ -4,21 +4,17 @@ from openTSNE import TSNE
 from umap import UMAP
 from typing import List
 
-from subtle.module import morlet_cwt
-from subtle.module import Phenograph
-
-class Data:
-    def __init__(self, X):
-        self.X = X
+from subtle.module import morlet_cwt, Data, Phenograph, run_DIB
 
 class Mapper:
-    def __init__(self, fs, embedding_method='umap', n_pca=100, n_train_frames=120000):
+    def __init__(self, fs, embedding_method='umap', n_train_frames=120000):
         self.fs=fs
         self.trained=False
         self.n_train_frames = n_train_frames
 
-        self.pca = PCA(n_pca)
-        self.embedding = UMAP()
+        self.pca = PCA(100)
+        self.umap = UMAP(n_neighbors=50, n_components=2)
+        self.pheno = Phenograph()
 
     def train(self, Xs):
         dataset = [Data(X) for X in Xs]
@@ -29,19 +25,44 @@ class Mapper:
         S = np.concatenate([data.spectrogram for data in dataset])
         S = np.random.permutation(S)[:self.n_train_frames]
         PC = self.pca.fit_transform(S)
-        Z = self.embedding.fit_transform(PC)
-        y = self.subcluster.fit_predict(Z)
+        Z = self.umap.fit_transform(PC)
+        y = self.pheno.fit_predict(Z)
+        self.subclusters = np.unique(y)
 
-        for X in Xs:
-            data = Data()
-            data.X = X
-            data.spectrogram = self.get_spectrogram(X)
-                
-            S = np.concatenate([data.S for data in dataset])
-        S = np.random.permutation(S)[:self.maxN]
-            data.pc = self.pca.fit_transform(data.spectrogram)
+        for data in dataset:
+            data.PC = self.pca.transform(data.S)
+            data.Z = self.umap.transform(data.PC)
+            data.y = self.get_spectrogram(data.Z)
+            data.TP, data.R = self.get_transition_probability(data.y)
+            data.lambda2 = np.abs(np.linalg.eig(data.TP)[0][1])
+            data.tau = -1 / np.log( data.lambda2 ) * 2
 
-            outputs.append(data)
+        print('Traning supercluster model...')
+        self.avg_tau = sum([data.tau for data in dataset])/len(dataset)
+        a = np.concatenate([data.y[:-int(self.avg_tau)] for data in dataset])
+        b = np.concatenate([data.y[int(self.avg_tau):] for data in dataset])
+        self.supclusters = run_DIB(a, b)
+
+        for data in dataset:
+            data.Y = np.array([list(map(lambda y:sup[y], data.y)) for sup in self.supclusters]).T
+
+        self.trained = True
+        return dataset
+
+    def __call__(self, Xs):
+        assert self.trained, 'Model not trained. Train the model first.'
+        
+        dataset = [Data(X) for X in Xs]        
+        for data in dataset:
+            data.S = self.cwt(data.X)
+            data.PC = self.pca.transform(data.S)
+            data.Z = self.embedding.transform(data.PC[:self.maxPC])
+            data.y = self.subcluster.predict(data.Z)
+            data.TP, data.R = self.get_transition_probability(data.y)
+            data.lambda2 = np.abs(np.linalg.eig(data.TP)[0][1])
+            data.tau = -1 / np.log( data.lambda2 ) * 2
+            data.Y = np.array([list(map(lambda y:sup[y], data.y)) for sup in self.supclusters]).T
+        return dataset
 
     def get_spectrogram(self, X, omega=5, n_channels=50):
         assert isinstance(X, np.ndarray), 'X should be numpy array'
@@ -56,3 +77,18 @@ class Mapper:
             cwts.append(cwt)
         cwts = np.hstack(cwts) # [n_frames, n_channels * n_features]
         return cwts
+
+    def get_transition_probability(self, transitions, tau=1):
+        states = self.subclusters
+        state2index = {k:i for i, k in enumerate(states)}
+        transitions = list(map(lambda x: state2index[x], transitions))
+        n = len(states)
+
+        M = np.zeros(shape=(n, n))
+        for (i,j) in zip(transitions[:-tau], transitions[tau:]):
+            M[i, j] += 1
+        retention_rate = normalize(M, norm='l1', axis=1).diagonal()
+        
+        np.fill_diagonal(M, 0)
+        transition_probability = normalize(M, norm='l1', axis=1)
+        return transition_probability, retention_rate
--- subtle/module.py
+++ subtle/module.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy import signal
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics.cluster import contingency_matrix
+import phenograph
 
 def morlet_cwt(x, fs, omega, n_channels):
     f_nyquist = fs/2
@@ -7,8 +10,9 @@ def morlet_cwt(x, fs, omega, n_channels):
     widths = omega*fs/(2*freq*np.pi)
     return np.abs(signal.cwt(x, signal.morlet2, widths, w=omega))
 
-import phenograph
-from sklearn.neighbors import KNeighborsClassifier
+class Data:
+    def __init__(self, X):
+        self.X = X
 
 class Phenograph:
     def __init__(
@@ -58,3 +62,177 @@ class Phenograph:
     
     def _predict(self, X):
         return self.neigh.predict(X)
+
+
+
+# matlab -> python
+# https://github.com/bermanlabemory/behavioral-evolution
+
+def deterministicInformationBottleneck(pXY, k, f0=None, beta=1, tol=1e-6, maxIter=1000):
+    if isinstance(pXY, list):
+        a = np.unique(pXY[0])
+        b = np.unique(pXY[1])
+        pXY = np.histogram2d(pXY[0], pXY[1], bins=(a,b))[0]
+    pXY = pXY / np.sum(pXY)
+    pX = np.sum(pXY, axis=0)
+    pY_X = pXY / pX
+    
+    s = pXY.shape
+    N = s[0]
+    M = s[1]
+    
+    if f0 is None:
+        f = np.random.randint(k, size=N)
+    else:
+        f = f0
+    
+    pT = np.zeros(k)
+    pY_T = np.zeros((k,M))
+    
+    for i in range(k):
+        pT[i] = np.sum(pX[f==i])
+    idx = pT > 0
+    HT = -np.sum(pT[idx]*np.log2(pT[idx]))
+    
+    for i in range(k):
+        if pT[i] > 0:
+            pY_T[i,:] = np.sum(pXY[f==i,:], axis=0) / pT[i]
+        else:
+            pY_T[i,:] = 0
+    pYT = pY_T * pT[:, np.newaxis]
+    pY = np.sum(pYT)
+    temp = pYT * np.log2(pYT / (pT[:, None]*pY))
+    IYT = np.sum(temp[~np.isnan(temp) & ~np.isinf(temp)])
+    
+    n = 1
+    while True:
+        previousJ = HT - beta*IYT
+        
+        DKLs = findListKLDivergences(pY_X.T, pY_T)
+        fMat = np.subtract(np.log2(pT), beta*DKLs[0])
+        
+        f = np.argmax(fMat, axis=1)
+        
+        for i in range(k):
+            pT[i] = np.sum(pX[f==i])
+        idx = pT > 0
+        HT = -np.sum(pT[idx]*np.log2(pT[idx]))
+        
+        for i in range(k):
+            if pT[i] > 0:
+                pY_T[i,:] = np.sum(pXY[f==i,:], axis=0) / pT[i]
+            else:
+                pY_T[i,:] = 0
+        pYT = pY_T*pT[:, np.newaxis]
+        pY = np.sum(pYT, axis=0)
+        temp = pYT * np.log2(pYT / (pT[:, None]*pY))
+        IYT = np.sum(temp[~np.isnan(temp) & ~np.isinf(temp)])
+
+        J = HT - beta*IYT
+
+        if abs(J-previousJ) < tol or n >= maxIter:
+            break
+        else:
+            n = n + 1
+
+    vals = np.unique(f)
+    if len(vals) < k:
+        for i in range(len(vals)):
+            f[f == vals[i]] = i
+        pY_T = pY_T[vals,:]
+        pT = pT[vals]
+
+    return f, IYT, HT, pY_T, pT
+
+def findListKLDivergences(data, data2):
+    logData = np.log2(data)
+    logData[np.isnan(logData) | np.isinf(logData)] = 0
+    
+    entropies = -np.sum(np.multiply(data, logData), axis=1)
+    logData2 = np.log2(data2)
+    logData2[np.isnan(logData2) | np.isinf(logData2)] = 0
+    
+    D = - np.dot(data, logData2.T)
+    
+    D = D - entropies[:, None]
+    
+    return D, entropies
+
+def findParetoFront(X):
+    d = len(X[0,:])
+    N = len(X[:,0])
+    
+    idx = np.zeros(N, dtype=bool)
+    temp = np.zeros((N,d), dtype=bool)
+    for i in range(N):
+        temp[:] = False
+        for j in range(d):
+            temp[:,j] = X[i,j] < X[:,j]
+        
+        if np.max(np.sum(temp, axis=1)) < d:
+            idx[i] = True
+    
+    return idx
+
+def assign_cluster(z, z_prev):
+    M = contingency_matrix(z_prev, z)
+    prev_labels = M.argmax(axis=1)
+    z_new = np.zeros_like(z)+len(np.unique(z))-1
+    for i, prev_label in enumerate(prev_labels):
+        z_new[z==prev_label]=i
+    return z_new
+
+def run_DIB(X, Y, N=1000, minClusters=2, maxClusters=30, minLogBeta=-1, maxLogBeta=4, readout=100):
+    
+    betas = np.zeros(N)
+    numClusters = np.zeros(N, dtype=int)
+    clusterings = [None]*N
+    IYTs = np.zeros(N)
+    HTs = np.zeros(N)
+    
+    a = np.unique(X)
+    b = np.unique(Y)
+    pXY = np.histogram2d(X, Y, bins=(len(a),len(b)))[0]
+    
+    for i in range(N):
+        betas[i] = 10**(minLogBeta + (maxLogBeta-minLogBeta)*np.random.rand())
+        k = minClusters + np.random.randint(maxClusters - minClusters)
+        clusterings[i],IYTs[i],HTs[i],_,_ = deterministicInformationBottleneck(pXY,k,None,betas[i],1e-7,2000)
+        numClusters[i] = len(np.unique(clusterings[i]))
+        if i%readout == 0:
+            print ('Calculating for Iteration #%6i out of %6i' % (i,N))
+    
+    idx = findParetoFront(np.vstack((-HTs, IYTs)).T)
+    clusterings = [clusterings[i] for i in np.where(idx)[0]]
+    IYTs = IYTs[idx]
+    HTs = HTs[idx]
+    numClusters = numClusters[idx]
+    
+    sortIdx = np.argsort(IYTs)
+    clusterings = [clusterings[i] for i in sortIdx]
+    IYTs = IYTs[sortIdx]
+    HTs = HTs[sortIdx]
+    numClusters = numClusters[sortIdx]
+    
+    idx = np.hstack((0,np.where(np.diff(IYTs) > 1e-10)[0]+1))
+    clusterings = [clusterings[i] for i in idx]
+    IYTs = IYTs[idx]
+    HTs = HTs[idx]
+    numClusters = numClusters[idx]
+    
+    clusterings = np.array(clusterings)
+    clusterValues = np.unique(numClusters)
+    clusterChoices = np.zeros(len(numClusters), dtype=bool)
+    for i in range(len(clusterValues)):
+        idx = np.where(numClusters == clusterValues[i])[0][-1]
+        clusterChoices[idx] = True
+
+    supclusters = []
+    for c in clusterings[clusterChoices]:
+        if len(np.unique(c)) == 1:
+            c_prev = c
+        else:
+            c = assign_cluster(c, c_prev)
+        supcluster = {x:c[x] for x in np.unique(X)}
+        supclusters.append(supcluster)
+    return supclusters
