4e968830cc123d547fbfaa30a58caa83fa8e9ee7
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-01-05 15:23:22 +0900

    edit

--- subtle/function.py
+++ subtle/function.py
@@ -3,4 +3,4 @@ from scipy import signal
 def morlet_cwt(x, fs, omega, n_channels):
     freq = np.linspace(fs/20, fs/2, n_channels)
     widths = omega*fs/(2*freq*np.pi)
-    return np.abs(signal.cwt(x, signal.morlet2, widths, w=omega)
+    return np.abs(signal.cwt(x, signal.morlet2, widths, w=omega))
