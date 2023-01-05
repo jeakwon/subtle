81e43fbaf236517ec452a5defed0ad677766a1dc
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-01-05 15:40:48 +0900

    update mapper

---
+++ subtle/mapper.py
@@ -0,0 +1,19 @@
+import numpy as np
+from subtle.function import morlet_cwt
+
+class Mapper:
+    def __init__(self, fs):
+        self.fs
+
+    def get_spectrogram(self, X, omega=5, n_channels=50):
+        assert isinstance(X, np.array), 'X should be numpy array'
+        assert X.ndim==2, 'dimension of X should be 2'
+        n_frames, n_features = X.shape
+
+        cwts = []
+        for i in range(num_features):
+            x = X[:, i]
+            cwt = morlet_cwt(x, fs=self.fs, omega, n_channels).T # [n_frames, n_channels]
+            cwts.append(cwt)
+        cwts = np.hstack(cwts) # [n_frames, n_channels * n_features]
+        return cwts
