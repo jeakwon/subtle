f6313d6923cc35c35837026cc8787a16ca6d811d
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-01-11 14:04:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='subtle',
-   version='1.0.2',
+   version='1.0.3',
    description='Spectrogram-UMAP-Based Temporal Link Embedding',
    author='Jea Kwon',
    author_email='onlytojay@gmail.com',
