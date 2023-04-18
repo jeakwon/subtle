fb84191b3a9d0c4c723906e58654d0964e570f34
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-04-18 16:15:27 +0900

    update kinematics

--- subtle/kinematics.py
+++ subtle/kinematics.py
@@ -1,6 +1,40 @@
 import numpy as np
 import pandas as pd
 
+avatar_configs={
+    'fs': 20,
+    'nodes':{
+        'anus': 2,
+        'chest': 3,
+        'lfoot': 5,
+        'lhand': 7,
+        'neck': 1,
+        'nose': 0,
+        'rfoot': 4,
+        'rhand': 6,
+        'tip': 8,
+    },
+    'edges':{
+        'fbody': [1, 3],
+        'hbody': [3, 2],
+        'head': [0, 1],
+        'larm': [5, 3],
+        'lleg': [5, 2],
+        'rarm': [6, 3],
+        'rleg': [4, 2],
+        'tail': [2, 8],
+    },
+    'angles':{
+        'body': [1, 3, 2],
+        'head': [0, 1, 3],
+        'larm': [1, 3, 7],
+        'lleg': [3, 2, 5],
+        'rarm': [1, 3, 6],
+        'rleg': [3, 2, 4],
+        'tail': [3, 2, 8],
+    },
+}
+
 class Kinematics:
     def __init__(self, fs, nodes={}, edges={}, angles={}):
         self.fs = fs
