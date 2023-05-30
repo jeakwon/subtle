ea580211448c1c3db65670d03ec65529e5806bab
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-05-30 20:13:35 +0900

    update subtle-kinematics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='subtle',
-   version='1.0.12',
+   version='1.0.13',
    description='Spectrogram-UMAP-Based Temporal Link Embedding',
    author='Jea Kwon',
    author_email='onlytojay@gmail.com',
